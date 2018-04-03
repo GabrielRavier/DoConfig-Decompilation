df7f48f4afd9cbb7b6490e69d315d588b5e3b942
Gabriel Ravier <31440290+GabrielRavier@users.noreply.github.com>
2018-04-03 18:58:27 +0200

    Here are the sources !!!

---
+++ Resource.h
@@ -0,0 +1,92 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DoConfig.rc
+//
+
+#define IDS_APP_TITLE			103
+
+#define IDR_MAINFRAME			128
+#define IDD_DOCONFIG_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_DOCONFIG			107
+#define IDI_SMALL				108
+#define IDC_DOCONFIG			109
+#define STRANGE_LAYOUT			121
+#define NORMAL_LAYOUT			122
+#define IDC_MYICON				2
+#define IDC_STATIC				-1
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
+
+#define Button_OK  							1
+#define  Button_Cancel 					 0x2
+#define  Button_Font_Dropdown_Box 		 0x3EF
+#define  Button_Use_Gamepad 			 0x3F0
+#define  Button_Restore_Defaults 		 0x3F3
+#define  Button_Jump_1 					 0x3F4
+#define  Button_Attack_1 				 0x3F5
+#define  Button_Weapon_Plus_1 			 0x3F6
+#define  Button_Weapon_Minus_1 			 0x3F7
+#define  Button_Item_1					 0x3F8
+#define  Button_Map_1					 0x3F9
+#define  Button_Jump_2					 0x3FA
+#define  Button_Attack_2				 0x3FB
+#define  Button_Weapon_Plus_2			 0x3FC
+#define  Button_Weapon_Minus_2			 0x3FD
+#define  Button_Item_2					 0x3FE
+#define  Button_Map_2					 0x3FF
+#define   Button_Jump_3					 0x400
+#define   Button_Attack_3				 0x401
+#define   Button_Weapon_Plus_3			 0x402
+#define   Button_Weapon_Minus_3			 0x403
+#define   Button_Item_3					 0x404
+#define   Button_Map_3					 0x405
+#define   Button_Jump_4					 0x406
+#define   Button_Attack_4				 0x407
+#define   Button_Weapon_Plus_4			 0x408
+#define   Button_Weapon_Minus_4			 0x409
+#define   Button_Item_4					 0x40A
+#define   Button_Map_4					 0x40B
+#define   Button_Jump_5					 0x40C
+#define   Button_Attack_5				 0x40D
+#define   Button_Weapon_Plus_5			 0x40E
+#define   Button_Weapon_Minus_5			 0x40F
+#define   Button_Item_5					 0x410
+#define   Button_Map_5					 0x411
+#define   Button_Jump_6					 0x412
+#define   Button_Attack_6				 0x413
+#define   Button_Weapon_Plus_6			 0x414
+#define   Button_Weapon_Minus_6			 0x415
+#define   Button_Item_6					 0x416
+#define   Button_Map_6					 0x417
+#define   Button_Jump_7					 0x418
+#define   Button_Attack_7				 0x419
+#define   Button_Weapon_Plus_7			 0x41A
+#define   Button_Weapon_Minus_7			 0x41B
+#define   Button_Item_7					 0x41C
+#define   Button_Map_7					 0x41D
+#define   Button_Jump_8					 0x424
+#define   Button_Attack_8				 0x425
+#define   Button_Weapon_Plus_8			 0x426
+#define   Button_Weapon_Minus_8			 0x427
+#define   Button_Item_8					 0x428
+#define   Button_Map_8					 0x429
+#define   Button_Z_Jump_X_Attack		 0x42A
+#define   Button_X_Jump_Z_Attack	 	 0x42B
+#define   Button_Screen_Resolution		 0x42E
+#define   Button_Arrows_Button_Layout	 0x42F
+#define   Button_Strange_Button_Layout	 0x430
+#define   Button_Jump_OK				 0x431
+#define   Button_Attack_OK 				 0x432
